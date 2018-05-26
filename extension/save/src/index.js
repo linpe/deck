51f28b81d121b7b0429f2089d151fa4f2c39876d
Peter Lin <linpeee@gmail.com>
2018-05-27 00:21:41 +1000

    Fix styles

--- extension/save/src/index.js
+++ extension/save/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './styles/variables.css';
 import './styles/fonts.css';
 import './styles/reset.css';
-import './styles/base.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
