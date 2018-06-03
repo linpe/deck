8e3b33d4b539216820e2eb0624e3df2fca6cb469
Peter Lin <linpeee@gmail.com>
2018-06-03 11:55:22 +1000

    Whoops

--- extension/save/src/App.js
+++ extension/save/src/App.js
@@ -118,7 +118,7 @@ class App extends React.PureComponent {
           ...prevState,
           signIn: {
             ...prevState.signIn,
-            error: 'error.message',
+            error: error.message,
             sending: false,
           },
         }));
